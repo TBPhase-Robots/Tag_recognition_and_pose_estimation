780dc33449d9a5a56e41005eefc93d6fcefec1df
TBExtent <ef18558@bristol.ac.uk>
2022-07-26 09:19:12 +0100

    markers fliped

--- marker_maker.py
+++ marker_maker.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 # import cv2, PIL
-from cv2 import aruco
+from cv2 import ROTATE_180, aruco
+import cv2
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pandas as pd
@@ -11,14 +12,19 @@ aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 
 def marker_to_int(marker_num):
     img = aruco.drawMarker(aruco_dict, marker_num, 8)
+    cv2.rotate(img, ROTATE_180, img)
     output = 0
     shift = 0
 
     for line in img[1:-1]:
         for pixel in line[1:-1]:
+            
             if pixel == 255:
                 output |= (1 << shift)
+                print(output)
             shift += 1
+
+
 
     return output
 
@@ -32,6 +38,7 @@ def main():
         plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
         ax.axis("off")
 
+    print("marker_to_int")
     print(marker_to_int(3))
 
     
